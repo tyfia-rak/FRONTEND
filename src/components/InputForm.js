05eb439517d3caf4aa17b1a0bc1cbbc6448b6cde
Sarobidy <sarobidy.rakotonarivo.1@gmail.com>
2024-09-23 10:13:09 +0300

    refactor: update the grid to version 2

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Grid, Checkbox, FormControlLabel, Button, TextField } from '@mui/material';
+import { Checkbox, FormControlLabel, Button, TextField, Grid2 } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
@@ -31,53 +31,53 @@ function InputForm({ onSubmit, initialData }) {
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={fr}>
       <form onSubmit={handleSubmit}>
-        <Grid container spacing={2} alignItems="center">
-          <Grid item xs={12} sm={6} md={3}>
+        <Grid2 container spacing={2} alignItems="center">
+          <Grid2 item xs={12} sm={6} md={3}>
             <FormControlLabel
               control={<Checkbox checked={formData.agregat} onChange={handleChange} name="agregat" />}
               label="Agrégat"
             />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          </Grid2>
+          <Grid2 item xs={12} sm={6} md={3}>
             <FormControlLabel
               control={<Checkbox checked={formData.tresorerie} onChange={handleChange} name="tresorerie" />}
               label="Trésorerie"
             />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          </Grid2>
+          <Grid2 item xs={12} sm={6} md={3}>
             <FormControlLabel
               control={<Checkbox checked={formData.immobilisations} onChange={handleChange} name="immobilisations" />}
               label="Immobilisations"
             />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          </Grid2>
+          <Grid2 item xs={12} sm={6} md={3}>
             <FormControlLabel
               control={<Checkbox checked={formData.obligations} onChange={handleChange} name="obligations" />}
               label="Obligations"
             />
-          </Grid>
-          <Grid item xs={12} sm={6}>
+          </Grid2>
+          <Grid2 item xs={12} sm={6}>
             <DatePicker
               label="De"
               value={formData.startDate}
               onChange={handleDateChange('startDate')}
               renderInput={(params) => <TextField {...params} fullWidth />}
             />
-          </Grid>
-          <Grid item xs={12} sm={6}>
+          </Grid2>
+          <Grid2 item xs={12} sm={6}>
             <DatePicker
               label="À"
               value={formData.endDate}
               onChange={handleDateChange('endDate')}
               renderInput={(params) => <TextField {...params} fullWidth />}
             />
-          </Grid>
-          <Grid item xs={12}>
+          </Grid2>
+          <Grid2 item xs={12}>
             <Button type="submit" variant="contained" color="primary">
               Mettre à jour
             </Button>
-          </Grid>
-        </Grid>
+          </Grid2>
+        </Grid2>
       </form>
     </LocalizationProvider>
   );
